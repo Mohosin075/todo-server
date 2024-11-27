45ac594d03722a060482f4f287048610d97243c6
Mohosin <mohosinali075@gmail.com>
2024-11-27 10:45:12 +0600

    my commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const port = 5000;
 
 const cors = require('cors');
 
